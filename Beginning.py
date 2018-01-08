b8e50887e1ff32c9bc1590b61d5d39410122cba0
David Trejo <30703946+davidulicio@users.noreply.github.com>
2018-01-08 15:59:15 -0300

    separación total
    Separación de variables

--- Beginning.py
+++ Beginning.py
@@ -7,19 +7,33 @@ DGF Uchile
 
 """
 import numpy as np
-tabla = 'C:\Users\David\Desktop\\rapanui_19941026_V01.dat'
+print 'Trabajo de datos para Easter Island'
+# print 'Ingrese ubicacion (ruta de acceso) al archivo'
+# tabla = input()
+tabla = 'C:\Users\David\Desktop\\rapanui_19941123_V01.dat'
 data = open(tabla, 'r')  # Lectura de datos
 datos = data.readlines()
 
-
 def traspaso(data):
     "Traspasa los datos para trabajarlos"
+    data = open(tabla, 'r')  # Lectura de datos
+    datos = data.readlines()
     lista = []
     lista2 = []
     for i in range(len(datos)):
         if i >= 21:
             lista.append(datos[i])
+    data.close()
     for value in lista:
         lis = np.array(value.split())
         lista2.append(lis)
+    lista = np.asarray(lista2)
+    height = lista[:, 0]
+    pression = lista[:, 1]
+    temperature = lista[:, 2]
+    u = lista[:, 3]
+    v = lista[:, 4]
+    theta = lista[:, 5]
+    theta_e = lista[:, 6]
+    Q = lista[:, 7]
 traspaso(datos)
