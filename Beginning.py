af2c8aa70d2bbdc288fa1baa1e2af7e8571660bf
David Trejo <30703946+davidulicio@users.noreply.github.com>
2018-01-11 11:02:46 -0300

    Fin
    Fin gráficos a espera de algún comentario

--- Beginning.py
+++ Beginning.py
@@ -88,26 +88,45 @@ def data_cleansing(h, p, tem, RH, O3mPa, O3ppbv, O3DU, u, v, th, the, Q):
         if float(O3mPa[i]) != 9000:
             O3.append(float(O3mPa[i]) * 10)
             h_O3.append(h[i])
-    return RHf, h_RH, Tf, h_T, O3, h_O3
+    U = []
+    h_u = []
+    for i in range(len(u)):
+        if float(u[i]) != 9000:
+            U.append(float(u[i]))
+            h_u.append(h[i])
+    V = []
+    h_v = []
+    for i in range(len(v)):
+        if float(v[i]) != 9000:
+            V.append(float(v[i]))
+            h_v.append(h[i])
+
+    return RHf, h_RH, Tf, h_T, O3, h_O3, U, h_u, V, h_v
 
 
-def graphs(h1, h2, h3, RH, tem, Oz, y, m, d):
+def graphs(h1, h2, h3, h4, h5, RH, tem, Oz, u, v, y, m, d):
     "Genera graficos"
-    plt.title('Rapa Nui (27° S, 109° W); Date: ' + repr(y) + '/' + repr(m) +
-              '/' + repr(d) + '; CR2 / DMC archive')
-    plt.figure(num=1)
-    plt.xlabel(' Temperature(Celcius)       Ozone(nbar)        RH%')
-    plt.ylabel('Altitude [km]')
-    plt.plot(RH, h1, 'k', label='RH%')  # h vs RH
-    plt.plot(tem, h2, 'r', label='Temperature in Celcius')  # h vs T
-    plt.plot(Oz, h3, 'b', label='Ozone(nbar)')  # h vs O3DU
-    plt.legend()
-    plt.show()
+    f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+    ax1.set_title('Rapa Nui (27° S, 109° W); Date: ' + repr(y) + '/' + repr(m) +
+              '/' + repr(d) + ';')
+    ax2.set_title(' CR2 / DMC archive')
+    ax1.set_xlabel('Temperature(Celcius)    Ozone(nbar)   RH%')
+    ax1.set_ylabel('Altitude [km]')
+    ax1.plot(RH, h1, 'k', label='RH%')  # h vs RH
+    ax1.plot(tem, h2, 'r', label='Temperature in Celcius')  # h vs T
+    ax1.plot(Oz, h3, 'b', label='Ozone(nbar)')  # h vs O3DU
+    ax2.plot(u, h4, 'b', label='Zonal Wind(u)')
+    ax2.plot(v, h5, 'r', label='Meridional Wind(v)')
+    ax2.set_xlim(-40, 80)
+    ax1.legend()
+    ax2.legend()
+    f.show()
 
 " USO DE LAS FUNCIONES "
 h, press, tem, RH, O3mPa, O3ppbv, O3DU, u, v, th, the, Q = data_transfer(datos)
-RHf, h_RH, Tf, h_T, O3, h_O3 = data_cleansing(h, press, tem,
-                                              RH, O3mPa, O3mPa, O3DU, u, v,
-                                              th, the, Q)
+RHf, h_RH, Tf, h_T, O3, h_O3, u, h_u, v, h_v = data_cleansing(h, press,
+                                                              tem, RH, O3mPa,
+                                                              O3mPa, O3DU, u,
+                                                              v, th, the, Q)
 y, m, d = date(tabla)
-graphs(h_RH, h_T, h_O3, RHf, Tf, O3, y, m, d)
+graphs(h_RH, h_T, h_O3, h_u, h_v, RHf, Tf, O3, u, v, y, m, d)
